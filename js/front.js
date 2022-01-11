5924d8667777f73f23f45be6ed5f1208769f0f62
shivamsapro <83757877+shivamsapro@users.noreply.github.com>
2022-01-11 11:57:49 +0545

    Add files via upload

---
+++ js/front.js
@@ -0,0 +1,90 @@
+$(function () {
+
+    // ---------------------------------------------- //
+    // Navbar
+    // ---------------------------------------------- //
+
+    $(document).scroll(function () {
+        if ($(window).scrollTop() >= $('header').offset().top) {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    });
+
+
+    // ---------------------------------------------- //
+    // Scroll Spy
+    // ---------------------------------------------- //
+
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 80
+    });
+
+    // ---------------------------------------------- //
+    // Preventing URL update on navigation link click
+    // ---------------------------------------------- //
+
+    $('.navbar-nav a, #scroll-down').bind('click', function (e) {
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 1000);
+        e.preventDefault();
+    });
+
+    // ------------------------------------------------------ //
+    // styled Google Map
+    // ------------------------------------------------------ //
+
+    map();
+
+
+    // ------------------------------------------------------ //
+    // For demo purposes, can be deleted
+    // ------------------------------------------------------ //
+
+    var stylesheet = $('link#theme-stylesheet');
+    $("<link id='new-stylesheet' rel='stylesheet'>").insertAfter(stylesheet);
+    var alternateColour = $('link#new-stylesheet');
+
+    if ($.cookie("theme_csspath")) {
+        alternateColour.attr("href", $.cookie("theme_csspath"));
+    }
+
+    $("#colour").change(function () {
+
+        if ($(this).val() !== '') {
+
+            var theme_csspath = 'css/style.' + $(this).val() + '.css';
+
+            alternateColour.attr("href", theme_csspath);
+
+            $.cookie("theme_csspath", theme_csspath, {
+                expires: 365,
+                path: document.URL.substr(0, document.URL.lastIndexOf('/'))
+            });
+
+        }
+
+        return false;
+    });
+
+});
+
+
+// ------------------------------------------------------ //
+// pdf link opened in lightbox
+// ------------------------------------------------------ //
+
+$(document).ready(function() {
+/* Apply fancybox to multiple items */
+
+$("a.iframe").fancybox({
+'width': 640, // or whatever you want
+'height': 480, // or whatever you want
+'type': 'iframe'
+});
+
+});
